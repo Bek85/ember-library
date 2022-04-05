98c4fe57ed756c64e0bcb92d272d34cadd0d3fc6
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-04-05 14:11:51 +0500

    initiate an ember app

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'book-app/app';
+import config from 'book-app/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
