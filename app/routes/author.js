856d476f2fd0216921ee2d7663f795a41147ccde
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-04-12 18:30:57 +0500

    fill author.hbs with content and render static data returned from its route

--- app/routes/author.js
+++ app/routes/author.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
 
-export default class AuthorRoute extends Route {}
+export default class AuthorRoute extends Route {
+  model() {
+    return [
+      { firstName: 'Alex', lastName: 'Smith' },
+      { firstName: 'Ben', lastName: 'Jackson' },
+    ];
+  }
+}
