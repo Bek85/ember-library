1984abba415e118bd7b4e7cb4a18c07650da3f90
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-04-19 23:53:18 +0500

    generate error substate template and replicate error

--- app/routes/author.js
+++ app/routes/author.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { Promise as EmberPromise } from 'rsvp';
+import { reject } from 'rsvp';
 
 function wait(delay) {
   return new EmberPromise(function (resolve) {
@@ -13,8 +14,12 @@ function wait(delay) {
 export default class AuthorRoute extends Route {
   @service dataService;
 
-  async model() {
-    await wait(2000);
-    return this.dataService.getAuthors();
+  // async model() {
+  //   await wait(2000);
+  //   return this.dataService.getAuthors();
+  // }
+
+  model() {
+    return reject(this.dataService.getAuthors());
   }
 }
