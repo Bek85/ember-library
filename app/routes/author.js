5da2c2c9367a1120400cb0eb8af6d92e29f11e23
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-04-19 23:47:14 +0500

    generate general loading substate

--- app/routes/author.js
+++ app/routes/author.js
@@ -1,10 +1,20 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { Promise as EmberPromise } from 'rsvp';
+
+function wait(delay) {
+  return new EmberPromise(function (resolve) {
+    setTimeout(function () {
+      resolve();
+    }, delay);
+  });
+}
 
 export default class AuthorRoute extends Route {
   @service dataService;
 
-  model() {
+  async model() {
+    await wait(2000);
     return this.dataService.getAuthors();
   }
 }
--- app/routes/books.js
+++ app/routes/books.js
@@ -1,3 +1,16 @@
 import Route from '@ember/routing/route';
+import { Promise as EmberPromise } from 'rsvp';
 
-export default class BooksRoute extends Route {}
+function wait(delay) {
+  return new EmberPromise(function (resolve) {
+    setTimeout(function () {
+      resolve();
+    }, delay);
+  });
+}
+
+export default class BooksRoute extends Route {
+  async model() {
+    return await wait(2000);
+  }
+}
